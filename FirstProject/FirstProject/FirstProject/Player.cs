79ca48726ac83f2b348249e694a0339b59fd4e18
Jens <ichunddu180@hotmail.de>
2017-04-11 10:08:46 +0200

    player class

---
+++ FirstProject/FirstProject/FirstProject/Player.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Graphics;
+
+namespace FirstProject
+{
+    class Player
+    {
+
+        //Falls ein Sprite geladen wird
+        private Texture texture;
+        private Sprite sprite = new Sprite();
+
+        //Falls ein Shape generiert wird
+        private RectangleShape rect;
+
+        public Player(Vector2u size, Vector2f position, int rotate = 0)
+        {
+            sprite.Position = position;
+            sprite.Rotation = rotate;
+        }
+
+        public Player(int width, int height, Vector2f position, Color color, int rotate = 0)
+        {
+            rect = new RectangleShape(new Vector2f(width, height));
+            rect.FillColor = color;
+            rect.Rotation = rotate;
+            rect.Position = position;
+        }
+
+        public void setTexture(String url)
+        {
+            texture = new Texture(new Image(url));
+            sprite.Texture = texture;
+        }
+
+        public void setColor(Color c)
+        {
+            sprite.Color = c;
+        }
+
+        public void setPositionSprite(Vector2f pos)
+        {
+            sprite.Position = pos;
+        }
+
+        public void setPositionRect(Vector2f pos)
+        {
+            rect.Position = pos;
+        }
+
+        public void setRotation(int rotate)
+        {
+            sprite.Rotation = rotate;
+        }
+
+        public Sprite getSprite()
+        {
+            return sprite;
+        }
+
+        public RectangleShape getRect()
+        {
+            return rect;
+        }
+
+    }
+}
--- FirstProject/FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/FirstProject/Program.cs
@@ -7,22 +7,63 @@ using System.Collections.Generic;
 using SFML;
 using SFML.Graphics;
 using SFML.Window;
+using SFML.System;
 
 namespace FirstProject
 {
     class Program
     {
+        public bool running = true;
+        private Player player;
+
         static void Main(string[] args)
         {
-            var rw = new RenderWindow(VideoMode.FullscreenModes[0], "Helloworld", Styles.Default);
-            bool running = true;
-            while(running)
+            Program game = new Program();
+            game.Run();
+        }
+
+        public void Run()
+        {
+            Initialize();
+
+            var window = new RenderWindow(new VideoMode(800,600), "Pong", Styles.Default);
+            DateTime previous_time = DateTime.Now;
+            DateTime current_time;
+            double delta;
+
+            while (running)
             {
-                rw.Clear(Color.Cyan);
-                rw.DispatchEvents();
-                rw.Closed += (sender, evtArgs) => running = false;
-                rw.Display();
+                current_time = DateTime.Now;
+                delta = current_time.Subtract(previous_time).TotalMilliseconds;
+                previous_time = current_time;
+
+                window.DispatchEvents();
+                window.Closed += (sender, evtArgs) => running = false;
+
+                Update(delta);
+
+                window.Clear(Color.Black);      
+                Render(window);
+                window.Display();
             }
+        }
+
+        private void Initialize()
+        {
+            //player = new Player(new Vector2u(20,20), new Vector2f(10,10));
+            //player.setTexture("C:/Users/Jens/Desktop/6. Semester/Programming Patterns/FirstProject/pall.png");
+            player = new Player(10, 100, new Vector2f(10, 200), Color.Green, 0);
+            Console.Write(player.getSprite());
+        }
+
+        private void Update(double delta)
+        {
+            //Console.WriteLine("delta: " + delta);
+        }
+
+        private void Render(RenderWindow window)
+        {
+            window.Draw(player.getRect());
         }
     }
 }
